8ba4b340003e6b8179432b3aa3cc602679900f19
Nolan Porter <nrp4137@rit.edu>
2024-10-02 18:54:10 -0400

    Fix for when to move logic

--- director/basic_director.py
+++ director/basic_director.py
@@ -8,7 +8,7 @@ class BasicDirector:
     # This method is called to process each frame
     def process_frame(self, frame : list, frameHeight: int, frameWidth: int):
     # Do something with the frame
-        print(frame)
+        #print(frame)
         if len(frame) > 0:
 
             #Use the frame height and width to calculate an acceptable box
@@ -32,8 +32,8 @@ class BasicDirector:
             x, y, w, h = first_face
 
             # Calculate the center of the bounding box
-            bbox_center_x = x + w // 2
-            bbox_center_y = y + h // 2
+            bbox_center_x = x + (w // 2)
+            bbox_center_y = y + (h // 2)
 
             #Are we inside the acceptable box
             if (bbox_center_x < acceptable_box_left or bbox_center_x > acceptable_box_right or
@@ -41,11 +41,11 @@ class BasicDirector:
 
                 #Move accordinly
                 #This is where it gets tricky, deciding how far to move the camera
-                if bbox_center_x < frame_center_x:
-                    print("Move camera left")
-                elif bbox_center_x > frame_center_x:
-                    print("Move camera right")
-                if bbox_center_y < frame_center_y:
-                    print("Move camera up")
-                elif bbox_center_y > frame_center_y:
-                    print("Move camera down")
+                if bbox_center_x < acceptable_box_left:
+                    print("Move camera left: " + "Bbox center x= " + str(bbox_center_x) + " acceptable left: " + str(acceptable_box_left))
+                elif bbox_center_x > acceptable_box_right:
+                    print("Move camera right: " + "Bbox center x= " + str(bbox_center_x) + " acceptable right: " + str(acceptable_box_right))
+                if bbox_center_y < acceptable_box_top:
+                    print("Move camera up: " + "Bbox center y= " + str(bbox_center_y) + " acceptable top: " + str(acceptable_box_top))
+                elif bbox_center_y > acceptable_box_bottom:
+                    print("Move camera down: " + "Bbox center y= " + str(bbox_center_y) + " acceptable bottom: " + str(acceptable_box_bottom))
