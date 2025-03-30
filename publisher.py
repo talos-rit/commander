50a14c17ff802083fabaafac3bd9c54720cc8e0d
Alex Vernes <adv3429@rit.edu>
2025-03-30 17:06:02 -0400

    Added polar and cart position saving and getting

--- publisher.py
+++ publisher.py
@@ -10,7 +10,7 @@ class Publisher:
     """
     connection = OperatorConnection(host=SOCKET_HOST, port=SOCKET_PORT)
     command_count = 0
-    ENCODING = 'utf-8'
+    CHAR_ENCODING = 'utf-8'
 
     @staticmethod
     def close_connection():
@@ -122,20 +122,19 @@ class Publisher:
         the Anchor value is ignored and the position is always treated as if anchor is set to false.
 
         Name    CHAR[]      Name descriptor for the position (non null terminated)
-        Anchor  BOOLEAN     Whether the position will move relative to the parent position
+        Anchor  BOOLEAN     Whether the position will move relative to the parent position (0x01 for True, 0x00 for False)
         Parent  CHAR[]      Another previously saved position to act as a parent (or refernce) position
         """
 
         name_len_bytes  = int_to_bytes(len(name), num_bits=8, unsigned=True)
-        name_bytes = name.encode(Publisher.ENCODING)
+        name_bytes = name.encode(Publisher.CHAR_ENCODING)
 
         anchor_bytes = b'\x01' if anchor else b'\x00'
 
         parent_len_bytes = int_to_bytes(len(parent), num_bits=8, unsigned=True)
-        parent_bytes = parent.encode(Publisher.ENCODING)
+        parent_bytes = parent.encode(Publisher.CHAR_ENCODING)
 
         payload = name_len_bytes + name_bytes + anchor_bytes + parent_len_bytes + parent_bytes
-
         Publisher.connection.publish(
             command=Command.SAVE_POSITION,
             payload=payload
@@ -153,7 +152,6 @@ class Publisher:
         name_bytes = name.encode(Publisher.ENCODING)
 
         payload = name_len_bytes + name_bytes
-
         Publisher.connection.publish(
             command=Command.DELETE_POSITION,
             payload=payload
@@ -170,12 +168,93 @@ class Publisher:
         name_bytes = name.encode(Publisher.ENCODING)
 
         payload = name_len_bytes + name_bytes
-
         Publisher.connection.publish(
             command=Command.GO_TO_POSITION,
             payload=payload
         )
+    
+    @staticmethod
+    def set_polar_position(name: str, delta: int, azimuth: int, radius: int):
+        """
+        Defines a position in terms of polar coordinates
 
+        Name        CHAR[]  Name descriptor for the position (non null terminated)
+        Delta       INT32   Tenths of degrees on delta axis 
+        Azimuth     INT32   Tenths of degrees on azimuth axis 
+        Radius      INT32   Tenths of distance to extend outwards 
+        """
+
+        name_len_bytes = int_to_bytes(len(name), num_bits=8, unsigned=True)
+        name_bytes = name.encode(Publisher.ENCODING)
+
+        delta_bytes = int_to_bytes(delta, num_bits=32, unsigned=False)
+        azimuth_bytes = int_to_bytes(azimuth, num_bits=32, unsigned=False)
+        radius_bytes = int_to_bytes(radius, num_bits=32, unsigned=False)
+
+        payload = name_len_bytes + name_bytes + delta_bytes + azimuth_bytes + radius_bytes
+        Publisher.connection.publish(
+            command=Command.SET_POLAR_POSITION,
+            payload=payload
+        )
+    
+    @staticmethod
+    def get_polar_position(name: str):
+        """
+        Returns the polar coordinates of a named position
+
+        Name    CHAR[]  Name descriptor for the position (non null terminated)
+        """
+        name_len_bytes = int_to_bytes(len(name), num_bits=8, unsigned=True)
+        name_bytes = name.encode(Publisher.CHAR_ENCODING)
+
+        payload = name_len_bytes + name_bytes
+
+        Publisher.connection.publish(
+            command=Command.GET_POLAR_POSITION,
+            payload=payload
+        )
+    
+    @staticmethod
+    def set_cartesian_position(name: str, x_mm_tenths: int, y_mm_tenths: int, z_mm_tenths: int):
+        """
+        Defines a position in terms of cartesian coordinates
+
+        Name    CHAR[]  Name descriptor for the position (non null terminated)
+        X       INT32   Tenths of millimeters on X-axis 
+        Y       INT32   Tenths of millimeters on Y-axis 
+        Z       INT32   Tenths of millimeters on Z-axis 
+        """
+
+        name_len_bytes = int_to_bytes(len(name), num_bits=8, unsigned=True)
+        name_bytes = name.encode(Publisher.CHAR_ENCODING)
+
+        x_mm_tenths_bytes = int_to_bytes(x_mm_tenths, num_bits=32, unsigned=False)
+        y_mm_tenths_bytes = int_to_bytes(y_mm_tenths, num_bits=32, unsigned=False)
+        z_mm_tenths_bytes = int_to_bytes(z_mm_tenths, num_bits=32, unsigned=False)
+
+
+        payload = name_len_bytes + name_bytes + x_mm_tenths_bytes + y_mm_tenths_bytes + z_mm_tenths_bytes
+        Publisher.connection.publish(
+            command=Command.SET_CARTESIAN_POSITION,
+            payload=payload
+        )
+    
+    @staticmethod
+    def get_cartesian_position(name: str):
+        """
+        Returns the cartesian coordinates of a named position
+
+        Name    CHAR[]  Name descriptor for the position (non null terminated)
+        """
+        name_len_bytes = int_to_bytes(len(name), num_bits=8, unsigned=True)
+        name_bytes = name.encode(Publisher.CHAR_ENCODING)
+
+        payload = name_len_bytes + name_bytes
+
+        Publisher.connection.publish(
+            command=Command.GET_CARTESIAN_POSITION,
+            payload=payload
+        )
 
 def main():
     while (not Publisher.connection.is_connected):
