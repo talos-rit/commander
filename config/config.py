c013bb67a47263398a459c1e9bcd8a57ce85e7ce
frey808 <kaifrazier413@gmail.com>
2025-09-30 17:53:24 -0400

    organized config

--- config/config.py
+++ config/config.py
@@ -1,13 +1,21 @@
 import os
+
 import yaml
 
 # remember to add a .local version to .gitignore when creating new config directories
 
 CAMERA_BASE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "camera_config.yaml")
-CAMERA_LOCAL_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "camera_config.local.yaml")
+CAMERA_LOCAL_CONFIG_PATH = os.path.join(
+    os.path.dirname(__file__), "camera_config.local.yaml"
+)
 
-NETWORK_BASE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "network_config.yaml")
-NETWORK_LOCAL_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "network_config.local.yaml")
+NETWORK_BASE_CONFIG_PATH = os.path.join(
+    os.path.dirname(__file__), "network_config.yaml"
+)
+NETWORK_LOCAL_CONFIG_PATH = os.path.join(
+    os.path.dirname(__file__), "network_config.local.yaml"
+)
+
 
 def load_a_config(base_path, local_path):
     def _load_yaml(path):
@@ -28,6 +36,7 @@ def load_a_config(base_path, local_path):
         return base
 
     return merge_dicts(base_config, local_config)
+
 
 CAMERA_CONFIG = load_a_config(CAMERA_BASE_CONFIG_PATH, CAMERA_LOCAL_CONFIG_PATH)
 NETWORK_CONFIG = load_a_config(NETWORK_BASE_CONFIG_PATH, NETWORK_LOCAL_CONFIG_PATH)
