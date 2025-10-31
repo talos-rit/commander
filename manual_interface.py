ac5e3faf5460984d92a8ddc590822848d56224fa
Hiro <hiroto125takeuchi@gmail.com>
2025-10-31 11:50:43 -0400

    fix: redefinition of method from merging

--- manual_interface.py
+++ manual_interface.py
@@ -231,13 +231,6 @@ class ManualInterface(tkinter.Tk):
 
         button.bind("<ButtonPress>", lambda event: self.start_move(direction))
         button.bind("<ButtonRelease>", lambda event: self.stop_move(direction))
-    
-    def draw_no_signal_display(self) -> ImageTk.PhotoImage:
-        no_signal_image = Image.new("RGB", (500, 380), color="gray")
-        draw = ImageDraw.Draw(no_signal_image)
-        draw.text((225, 180), "No Signal", fill="white")
-        image_tk = ImageTk.PhotoImage(no_signal_image)
-        return image_tk
 
     def draw_no_signal_display(self) -> ImageTk.PhotoImage:
         no_signal_image = Image.new("RGB", (500, 380), color="gray")
