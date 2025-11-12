f451496cf4fd0acb0766156528ddf92de5e60447
Hiro <hiroto125takeuchi@gmail.com>
2025-11-12 18:32:54 -0500

    fix: cancel in iterative task, segfault when holding process

--- tkscheduler.py
+++ tkscheduler.py
@@ -3,19 +3,25 @@ from tkinter import Tk
 
 class IterativeTask:
     root: Tk
-    running = True
+    is_running = True
+    _stop_flag = False
+    _call_id: str | None = None
 
     def __init__(self, root: Tk, ms, func, *args):
         self.root = root
         self.iterative_call(int(ms), func, *args)
 
     def iterative_call(self, ms, func, *args):
-        self.root.after(ms, func, *args)
-        if self.running:
+        self._call_id = self.root.after(ms, func, *args)
+        if not self._stop_flag:
             self.root.after(ms, self.iterative_call, ms, func, *args)
+        else:
+            self.is_running = False
 
     def cancel(self):
-        self.running = False
+        self._stop_flag = True
+        if self._call_id is not None:
+            self.root.after_cancel(self._call_id)
 
 
 class Scheduler:
