b15cb6d253b8e76c79bd3895ec355d3571982466
Hiro <hiroto125takeuchi@gmail.com>
2026-01-19 18:45:34 -0500

    fix: remove import run code

--- talos.py
+++ talos.py
@@ -2,38 +2,11 @@ import argparse
 import multiprocessing
 import multiprocessing.managers
 import sys
-from pathlib import Path
 
 from loguru import logger
 
 from src.textual_tui.main_interface import TextualInterface
 from src.tk_gui.main_interface import TKInterface
-
-log_dir = Path(".log")
-if not log_dir.exists():
-    log_dir.mkdir()
-
-logger.add(
-    ".log/log_{time}.log",
-    enqueue=True,
-    retention="10 days",
-    backtrace=True,
-    diagnose=True,
-)
-
-
-class StreamToLoguru:
-    def write(self, message):
-        message = message.strip()
-        if message:
-            logger.info(message)
-
-    def flush(self):
-        pass  # Needed for file-like API
-
-
-sys.stdout = StreamToLoguru()
-sys.stderr = StreamToLoguru()
 
 
 def configure_logger():
