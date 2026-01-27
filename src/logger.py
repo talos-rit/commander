ae6ab2e9606ac2a0b75805dd964f2d74d34a8cac
Hiro <hiroto125takeuchi@gmail.com>
2026-01-27 17:58:31 -0500

    fix: import error

---
+++ src/logger.py
@@ -0,0 +1,26 @@
+import sys
+
+from loguru import logger
+
+
+def configure_logger(remove_existing: bool = False, process_name: str = "log"):
+    if remove_existing:
+        # Shuts up console output for loguru
+        logger.remove()
+    logger.add(
+        f".log/{process_name}_{{time}}.log",
+        enqueue=True,
+        level="DEBUG",
+    )
+
+    class StreamToLoguru:
+        def write(self, message):
+            message = message.strip()
+            if message:
+                logger.info(message)
+
+        def flush(self):
+            pass  # Needed for file-like API
+
+    sys.stdout = StreamToLoguru()
+    sys.stderr = StreamToLoguru()
