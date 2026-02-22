6799560ebec4fbeb5377bcf977ff02263de8c5ff
cwo3990 <cwo3990@rit.edu>
2026-02-21 19:42:57 -0500

    fixed issue where one function was there twice after merge with main

--- talos.py
+++ talos.py
@@ -26,12 +26,6 @@ def run_server(app: App):
     endpoint = TalosEndpoint(app)
     endpoint.run()
 
-
-def run_server(app: App):
-    endpoint = TalosEndpoint(app)
-    endpoint.run()
-
-
 def terminal_interface(args=None):
     configure_logger(True)
     smm = multiprocessing.managers.SharedMemoryManager()
