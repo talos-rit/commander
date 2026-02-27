e9a6495ca9f6693ad353f2c7316384a99fb4258c
cwo3990 <cwo3990@rit.edu>
2026-02-26 22:16:09 -0500

    log level now affects stderr for tkinter gui

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,18 @@
+import sys
+
 from loguru import logger
 
 from .config.load import APP_SETTINGS
 
 
 def configure_logger(remove_existing: bool = False, process_name: str = "log"):
-    if remove_existing:
-        # Shuts up console output for loguru
-        logger.remove()
+    logger.remove()
+    if not remove_existing:
+        logger.add(
+            sys.stderr,
+            enqueue=True,
+            level=APP_SETTINGS.log_level,
+        )
     logger.add(
         f".log/{process_name}_{{time}}.log",
         enqueue=True,
